1001230517a1b29722e50860cd156fe7b9544b57
Sarah Shader <sarahshader@gmail.com>
2025-02-10 10:32:22 -0800

    Update UI for Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,54 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
 import { api } from "../convex/_generated/api";
 import Link from "next/link";
+import { SignUpButton } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
-        Convex + Next.js
+        Convex + Next.js + Clerk
+        <UserButton />
       </header>
-      <main className="p-8 flex flex-col gap-16">
-        <h1 className="text-4xl font-bold text-center">Convex + Next.js</h1>
-        <Content />
+      <main className="p-8 flex flex-col gap-8">
+        <h1 className="text-4xl font-bold text-center">
+          Convex + Next.js + Clerk
+        </h1>
+        <Authenticated>
+          <Content />
+        </Authenticated>
+        <Unauthenticated>
+          <SignInForm />
+        </Unauthenticated>
       </main>
     </>
+  );
+}
+
+function SignInForm() {
+  return (
+    <div className="flex flex-col gap-8 w-96 mx-auto">
+      <p>Log in to see the numbers</p>
+      <SignInButton mode="modal">
+        <button className="bg-foreground text-background px-4 py-2 rounded-md">
+          Sign in
+        </button>
+      </SignInButton>
+      <SignUpButton mode="modal">
+        <button className="bg-foreground text-background px-4 py-2 rounded-md">
+          Sign up
+        </button>
+      </SignUpButton>
+    </div>
   );
 }
 
@@ -54,7 +88,7 @@ function Content() {
         Numbers:{" "}
         {numbers?.length === 0
           ? "Click the button!"
-          : numbers?.join(", ") ?? "..."}
+          : (numbers?.join(", ") ?? "...")}
       </p>
       <p>
         Edit{" "}
